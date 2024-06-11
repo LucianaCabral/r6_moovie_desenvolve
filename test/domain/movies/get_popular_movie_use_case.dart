74f2168a49911a350343b01b13218fe5c3b94db6
LucianaCabral <eng.luciana.cabral@gmail.com>
2024-06-11 20:41:22 -0300

    :test_tube: test: add units test movie

---
+++ test/domain/movies/get_popular_movie_use_case.dart
@@ -0,0 +1,30 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mocktail/mocktail.dart';
+import 'package:r6_moovie_app/domain/repository/movie_repository.dart';
+import 'package:r6_moovie_app/domain/usecase/movies/get_popular_movie_use_case.dart';
+
+import '../../stubs/utils/class_mocks.dart';
+import '../../stubs/utils/stubs.dart';
+
+void main() {
+  late MoviesRepository moviesRepository;
+  late GetPopularMoviesUseCase getPopularMoviesUseCase;
+
+  final movie = moviesMock;
+
+  setUpAll(() {
+    moviesRepository = MoviesRepositoryMock();
+    getPopularMoviesUseCase = GetPopularMoviesUseCase(
+      moviesRepository,
+    );
+  });
+  group("GetPopularMovieUseCase: ", () {
+    test('Should return a list of Movie', () async {
+      when(() => moviesRepository.getPopularMovies()).thenAnswer((_) => Future.value([movie]));
+
+      final result = await getPopularMoviesUseCase.invoke();
+
+      expect(result, [movie]);
+    });
+  });
+}
